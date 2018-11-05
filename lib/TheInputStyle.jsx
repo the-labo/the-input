a4daf11000d58e3f2637be06a0f31c8b8c500574
okunishinishi <okunishinishi@gmail.com>
2018-11-05 15:18:45 +0900

    Releasing new package

--- lib/TheInputStyle.jsx
+++ lib/TheInputStyle.jsx
@@ -7,6 +7,7 @@ import { asStyleData, colorAlpha } from 'the-component-util'
 import { TheStyle } from 'the-style'
 import TheInputDateStyleData from './styleData/TheInputDateStyleData'
 import TheInputTextareaStyleData from './styleData/TheInputTextareaStyleData'
+import TheInputTextStyleData from './styleData/TheInputTextStyleData'
 import TheInputUploadStyleData from './styleData/TheInputUploadStyleData'
 
 /** Style for TheInput */
@@ -105,70 +106,15 @@ TheInputStyle.data = (options) => {
         padding: 0,
       },
     }),
-    asStyleData('.the-input-text', {
-      '.the-input-text-input': {
-        border: `1px solid ${inputBorderColor}`,
-        borderRadius: '2px',
-        boxShadow: `1px 1px 1px ${inputShadowColor} inset`,
-        boxSizing: 'border-box',
-        display: 'block',
-        minHeight: '28px',
-        outlineColor: dominantColor,
-        padding: '4px 8px',
-        width: '100%',
-      },
-      '.the-input-text-input-wrap': {
-        alignItems: 'center',
-        display: 'flex',
-        flexGrow: 1,
-        justifyContent: 'stretch',
-        whiteSpace: 'nowrap',
-      },
-      '.the-input-text-option': {
-        '&:last-child': {
-          borderBottom: 'none',
-        },
-        '&.the-input-text-option-selected': {
-          backgroundColor: colorAlpha(dominantColor, 0.2),
-        },
-        alignItems: 'center',
-        backgroundColor: backgroundColor,
-        borderBottom: `1px solid ${lightBorderColor}`,
-        boxSizing: 'border-box',
-        cursor: 'pointer',
-        display: 'flex',
-        justifyContent: 'flex-start',
-        minHeight: tappableHeight,
-        overflow: 'hidden',
-        padding: contentPadding,
-        textOverflow: 'ellipsis',
-      },
-      '.the-input-text-options': {
-        backgroundColor,
-        border: `1px solid ${lightBorderColor}`,
-        borderRadius: '0 0 2px 2px',
-        boxShadow: '2px 2px 4px rgba(0,0,0,0.33)',
-        boxSizing: 'border-box',
-        left: 0,
-        listStyle: 'none',
-        margin: '-1px 0 0',
-        maxHeight: '50vh',
-        overflow: 'auto',
-        padding: 0,
-        position: 'absolute',
-        right: 0,
-        top: '100%',
-        zIndex: 8,
-      },
-      '&': {
-        boxSizing: 'border-box',
-        display: 'inline-block',
-        margin: '0 4px',
-        maxWidth: contentWidth,
-        position: 'relative',
-        verticalAlign: 'middle',
-        width: '100%',
-      },
+    TheInputTextStyleData({
+      backgroundColor,
+      contentPadding,
+      contentWidth,
+      dominantColor,
+      inputBorderColor,
+      inputShadowColor,
+      lightBorderColor,
+      tappableHeight,
     }),
     asStyleData('.the-input-password', {
       '.the-input-password-toggle': ToggleIconStyle({
---
+++ test/TheInputTextStyleDataTest.jsx
@@ -0,0 +1,27 @@
+/**
+ * Test for TheInputTextStyleData.
+ * Runs with mocha.
+ */
+'use strict'
+
+import TheInputTextStyleData from '../lib/styleData/TheInputTextStyleData'
+import React from 'react'
+import { ok, equal } from 'assert'
+import { render } from 'the-script-test'
+
+describe('the-input-text-style-data', () => {
+  before(() => {
+  })
+
+  after(() => {
+  })
+
+  it('Render a component', () => {
+    let element = render(
+       <TheInputTextStyleData />
+    )
+    ok(element)
+  })
+})
+
+/* global describe, before, after, it */
